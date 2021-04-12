6755b772c57afc9332afe9cb30a9e71a636e0667
Hans Tsang <hans.tsang@chunwo.com>
2021-04-12 14:35:19 +0800

    update to bbc radio

--- playback.js
+++ playback.js
@@ -1,28 +1,25 @@
 const sources = [
-  "http://stream-dc1.radioparadise.com/aac-320", // paradise
-  "http://radiogong-ais-edge-3073-fra-eco-cdn.cast.addradio.de/radiogong/live/mp3/high?ar-distributor=f0b7&aw_0_req.gdpr=true&_art=dj0yJmlwPTYyLjU3LjIuMjcmaWQ9aWNzY3hsLXMzeGRnenhtYiZ0PTE2MTAzODM3MDcmcz03ODY2ZjI5YyNiNzcwYWViOGUxZWU0NWI2MWJlNTAzZWE4OWUyZjQwYw", // party gong
-  "http://ice2.somafm.com/folkfwd-128-mp3", // folk forward
-  "http://a.files.bbci.co.uk/media/live/manifesto/audio/simulcast/hls/nonuk/sbr_low/ak/bbc_radio_one.m3u8" // punk fm
+  "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_one", // BBC - Radio 1
+  "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_two", // BBC - Radio 1
+  "http://stream.live.vc.bbcmedia.co.uk/bbc_radio_three", // BBC - Radio 1
 ];
 
 const labels = [
-  [ "Radio Paradise", "https://www.radioparadise.com" ],
-  [ "Party Gong", "https://www.radiogong.de/"],
-  [ "SomaFM Folk Forward", "http://somafm.com/folkfwd/"],
-  [ "Punk FM", " http://www.punkfm.co.uk/"]
+  ["BBC - Radio 1", " http://www.bbc.co.uk/"],
+  ["BBC - Radio 2", " http://www.bbc.co.uk/"],
+  ["BBC - Radio 3", " http://www.bbc.co.uk/"],
 ];
 
 let playingIndex = 0; // current radio
 let playing = true; // stream status
 let music = null;
 
-setTimeout(function(){
-    loadStream(playingIndex);
+setTimeout(function () {
+  loadStream(playingIndex);
 }, 1);
 
-function loadStream(index){
-  if(playing && music !== null)
-    destroyStream();
+function loadStream(index) {
+  if (playing && music !== null) destroyStream();
 
   music = new Audio();
   music.src = sources[index];
@@ -34,34 +31,43 @@ function loadStream(index){
   playing = true;
 }
 
-function destroyStream(){
+function destroyStream() {
   music.pause();
   music.src = "";
   playIcon();
   playing = false;
 }
 
-function changePlayback(){
-  if(playing){ destroyStream();  }else{  loadStream(playingIndex);  }
+function changePlayback() {
+  if (playing) {
+    destroyStream();
+  } else {
+    loadStream(playingIndex);
+  }
 }
 
-function setLabel(index){
-  document.getElementById("label").innerHTML = '<h6> <a target="_blank" href="' + labels[index][1] + '">' + labels[index][0] + '</a></h6>';
+function setLabel(index) {
+  document.getElementById("label").innerHTML =
+    '<h6> <a target="_blank" href="' +
+    labels[index][1] +
+    '">' +
+    labels[index][0] +
+    "</a></h6>";
 }
 
-document.onkeydown = function(e) {
+document.onkeydown = function (e) {
   e = e || window.event;
-  switch(e.which || e.keyCode) {
+  switch (e.which || e.keyCode) {
     case 32:
       changePlayback();
-    break;
+      break;
   }
 };
 
 // icons
-function pauseIcon(){
-  document.getElementById('playbackButton').className = 'icon fa-pause';
+function pauseIcon() {
+  document.getElementById("playbackButton").className = "icon fa-pause";
 }
-function playIcon(){
-  document.getElementById('playbackButton').className = 'icon fa-play';
+function playIcon() {
+  document.getElementById("playbackButton").className = "icon fa-play";
 }
